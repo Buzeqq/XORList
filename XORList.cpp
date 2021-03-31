5453950903a87301c7766a58788a9296de5e91da
buzeqq <miloszchoj@gmail.com>
2021-03-31 13:58:43 +0200

    XOR List initial

---
+++ XORList.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Milosz Chojnacki on 31.03.2021.
+//
+
+#include "XORList.h"
---
+++ XORList.h
@@ -0,0 +1,8 @@
+//
+// Created by Milosz Chojnacki on 31.03.2021.
+//
+
+#ifndef XORLIST_XORLIST_H
+#define XORLIST_XORLIST_H
+
+#endif //XORLIST_XORLIST_H
